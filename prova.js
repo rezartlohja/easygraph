49d59a0ab86f20bdc5ed76ecbcdc21ef009a6647
rezart <rezart.lohja@gmail.com>
2017-08-22 15:43:43 +0200

    all

---
+++ prova.js
@@ -0,0 +1,13 @@
+EasyGraph = {
+    'load': function (){// la funzione che seleziona gli elementi easygraph dal documento HTML
+    'draw': { // tutte le funzioni di disegno dei vari tipi di grafici (es.: 'bar','line'...)
+
+    },
+    'filter':{// tutte le funzioni di filtro (es.: 'frequencyDistribution',)
+    },
+    'check' : {// tutte le funzioni di check dei vari tipi di grafici (es.: 'bar','line'...)
+
+    },
+    'utility':{// funzioni di utility per varie necessità interne della libreria
+    }
+}
